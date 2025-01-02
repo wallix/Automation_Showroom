65a463a3319404c3a471e688fbbce25a7e54c054
Swan CORTET <swcortet@wallix.com>
2025-01-02 16:12:55 +0100

    feat(UseCase3) V1 of UseCase3, basics

--- Terraform/Provisionning/02-Advanced/Using_Inventory/UseCase3 - OneAuthorizationPerTargetService/secrets.tf
+++ Terraform/Provisionning/02-Advanced/Using_Inventory/UseCase3 - OneAuthorizationPerTargetService/secrets.tf
@@ -2,9 +2,16 @@
 locals {
   primary_accounts = {
     for user_key, user in local.yaml_inventory["users_inventory"] :
-    user["user_name"] => "${user["user_name"]}-primary-password"
+    user_key => "${user_key}-primary-password"
   }
+
+  secondary_accounts = flatten([
+    for vm_key, vm in local.yaml_inventory["vms_inventory"] : [
+      for account in vm.accounts : "${vm_key}-${account}-password" # For password it's better to use "-" separtor if you want to store them in Azure Vault 
+    ]
+  ])
 }
+
 
 ### Generate primary_accounts passwords
 resource "random_password" "primary_accounts" {
@@ -18,15 +25,14 @@ resource "random_password" "primary_accounts" {
   override_special = "_@+.-"
 }
 
-
-# # Generate SSH Key
-# resource "tls_private_key" "rsa-4096" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
-# # write local file
-# resource "local_file" "private_key" {
-#   content         = tls_private_key.rsa-4096.private_key_pem
-#   filename        = "ssh_private_key.pem"
-#   file_permission = "0600"
-# }
+### Generate secondary_accounts passwords
+resource "random_password" "secondary_accounts" {
+  for_each         = toset(local.secondary_accounts)
+  length           = 16
+  min_lower        = 2
+  min_upper        = 2
+  min_numeric      = 2
+  min_special      = 2
+  special          = true
+  override_special = "_@+.-"
+}
